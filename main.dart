bdf0ddb8a1f508ba5f38f359035f488a5d0f34fe
Sebastian Avila <134023565+Sebastian1996avila@users.noreply.github.com>
2024-05-28 17:17:16 -0500

    Update main.dart
    main

--- main.dart
+++ main.dart
@@ -1,80 +1,28 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+import 'gallery_page.dart';
+import 'facts_page.dart'; // Asegúrate de importar correctamente la página de hechos
 
-class FactsPage extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('The Llamas for sale '),
-        backgroundColor: Color.fromRGBO(255, 235, 59, 1),
-        toolbarHeight: 100,
+    return MaterialApp(
+      title: 'LlamaParadise',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
       ),
-       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 20),
-              ListTile(
-                leading: Icon(Icons.api, size: 30, color: Colors.orange),
-                title: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'LLama Cobaya de las Montañas',
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Text(
-                      '800 USD',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ],
-                ),
-                trailing: ElevatedButton(
-                  onPressed: () {
-                    String productPrice = '800 USD';
-                    int price = int.tryParse(productPrice.split(' ')[0]) ?? 0;
-                    print(price); // Esto imprimirá 800
-                    // Puedes usar 'price' para la lógica del carrito
-                  },
-                  child: Text('Añadir al carrito'),
-                ),
-              ),
-              SizedBox(height: 20),
-              ListTile(
-                leading: Icon(Icons.api, size: 30, color: Colors.green),
-                title: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'LLama Americana',
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Text(
-                      '1000 USD',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                  ],
-                ),
-                trailing: ElevatedButton(
-                  onPressed: () {
-                    String productPrice = '1000 USD';
-                    int price = int.tryParse(productPrice.split(' ')[0]) ?? 0;
-                    print(price); // Esto imprimirá 1000
-                    // Puedes usar 'price' para la lógica del carrito
-                  },
-                  child: Text('Añadir al carrito'),
-                ),
-              ),
-              // Añade más ListTiles aquí según sea necesario
-            ],
-          ),
-        ),
-      ),
+      initialRoute: '/home', // Ruta inicial de la aplicación
+      routes: {
+        '/home': (context) => LoginPage(), // Cambiado a LoginPage como página de inicio
+        '/gallery': (context) => GalleryPage(),
+        '/facts': (context) => FactsPage(), 
+        '/home_page': (context) => HomePage()
+      },
     );
   }
 }
