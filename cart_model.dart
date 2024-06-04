5a75cd479289dbc6f8056590212a925f7a87413f
Sebastian Avila <134023565+Sebastian1996avila@users.noreply.github.com>
2024-06-04 01:00:26 -0500

    Update cart_model.dart

--- cart_model.dart
+++ cart_model.dart
@@ -2,6 +2,12 @@ import 'package:flutter/foundation.dart';
 
 class CartModel extends ChangeNotifier {
   final List<String> _items = [];
+  final Map<String, double> _itemPrices = {
+    'Llama Cobaya de las Montañas': 800.0 ,
+    'Llama Americana': 1000.0,
+    'Llama Andina Real': 1200.0,
+    'Llama Patagonica': 950.0,
+  };
 
   List<String> get items => _items;
 
@@ -14,4 +20,6 @@ class CartModel extends ChangeNotifier {
     _items.remove(item);
     notifyListeners();
   }
+
+  double get totalPrice => _items.fold(0, (total, item) => total + (_itemPrices[item] ?? 0));
 }
