48a46efe45e9a064b188f0609b9e37c01c7574f0
Sebastian Avila <134023565+Sebastian1996avila@users.noreply.github.com>
2024-06-04 00:58:56 -0500

    Update cart_page.dart

--- cart_page.dart
+++ cart_page.dart
@@ -10,17 +10,62 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Cart'),
+        backgroundColor: Colors.yellow[700],
+        toolbarHeight: 100,
       ),
-      body: ListView.builder(
-        itemCount: cart.items.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(cart.items[index]),
-          trailing: IconButton(
-            icon: Icon(Icons.remove),
-            onPressed: () {
-              cart.remove(cart.items[index]);
-            },
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.separated(
+                itemCount: cart.items.length,
+                itemBuilder: (context, index) => ListTile(
+                  leading: Icon(Icons.shopping_cart, color: Colors.yellow[700], size: 30),
+                  title: Text(
+                    cart.items[index],
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.remove_circle, color: Colors.red),
+                    onPressed: () {
+                      cart.remove(cart.items[index]);
+                    },
+                  ),
+                ),
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total Price:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '\$${cart.totalPrice.toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                onPrimary: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                textStyle: TextStyle(fontSize: 18),
+              ),
+              onPressed: () {
+                // Acción de pago
+              },
+              child: Text('Proceed to Checkout'),
+            ),
+          ],
         ),
       ),
     );
