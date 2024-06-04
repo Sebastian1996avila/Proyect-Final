70552e795b5b835e7e9df72467c6c46b07832309
Sebastian Avila <134023565+Sebastian1996avila@users.noreply.github.com>
2024-06-04 00:55:13 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -7,12 +7,15 @@ import 'gallery_page.dart';
 import 'facts_page.dart';
 import 'cart_model.dart';
 import 'cart_page.dart';
+import 'contacts_page.dart';
+import 'theme_notifier.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => CartModel()),
+        ChangeNotifierProvider(create: (context) => ThemeNotifier()), 
        // ChangeNotifierProvider(create: (context) => GalleryModel()),
       ],
       child: MyApp(),
@@ -23,18 +26,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
     return MaterialApp(
       title: 'LlamaParadise',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
-      initialRoute: '/home', 
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeNotifier.themeMode, // Usar el modo de tema del ThemeNotifier
+      initialRoute: '/home',
       routes: {
         '/home': (context) => LoginPage(),
         '/gallery': (context) => GalleryPage(),
         '/facts': (context) => FactsPage(),
         '/home_page': (context) => HomePage(),
         '/cart': (context) => CartPage(),
+        '/contacts': (context) => ContactsPage(),
       },
     );
   }
